a9acd9d7a98f6d963d6883276de9a0e0273dceaa
Andrei Serban <aiserban@outlook.com>
2022-09-27 20:49:02 +0300

    renamed app to server

--- server.ts
+++ server.ts
@@ -5,15 +5,7 @@ const app = express()
 const port = 3005
 
 app.get('/', async (req, res) => {
-  let str = '';
-
-  await Db.findMany({}).then((results) => {
-    for (let i = 0; i < results.length; i++) {
-      str = str.concat(results[i].street);
-    }
-  })
-
-  res.send('Hello World! <br>' + str)
+  res.send('Hello World! <br>')
 })
 
 app.listen(port, () => {
