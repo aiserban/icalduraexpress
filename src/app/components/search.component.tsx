2f243d9ba13680624195edbba5a4491fa88ec5e6
Andrei Serban <aiserban@outlook.com>
2022-09-28 23:09:10 +0300

    nvm using a dl component

--- src/app/components/search.component.tsx
+++ src/app/components/search.component.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useRef } from "react";
+import Select from "react-select";
 
 export default function Search() {
     const [searchInput, setSearchInput] = useState('abc');
-
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ]
     return (
         <>
-            <input placeholder="Street name" autoComplete="none" value={searchInput} onInput={event => setSearchInput((event.target as HTMLInputElement).value)}></input>
+            <Select options={options}></Select>
         </>
     )
 }
