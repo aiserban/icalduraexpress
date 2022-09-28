319674e29bbde0d11b7a93a85481ea18a4a15d2f
Andrei Serban <aiserban@outlook.com>
2022-09-28 22:57:29 +0300

    search component start

--- server.ts
+++ server.ts
@@ -8,14 +8,14 @@ const port = 3005
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "dist"));
-});
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "dist"));
+// });
 
-app.get('*', async (req, res) => {
+app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 })
 
 app.listen(port, () => {
   console.log(`iCaldura listening on port ${port}`)
-})  
+})
