6c8523af7635afee8c5f9c5db779d4fd20e0a47f
Andrei Serban <aiserban@outlook.com>
2022-09-29 15:25:24 +0300

    async select working

--- src/app/components/search.component.tsx
+++ src/app/components/search.component.tsx
@@ -1,25 +1,24 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import Select from 'react-select';
+import AsyncSelect from "react-select/async";
+// import Select from 'react-select';
+import Async, { useAsync } from 'react-select/async';
 
 export default function Search() {
     const [searchInput, setSearchInput] = useState('');
-    const [options, setOptions] = useState([{value: '', label: ''}]);
 
-    useEffect(() => {
-        axios.get(`http://localhost:3005/api/street/${searchInput}`,).then(res => {
-            const matchingStreets = [];
-            for (let i=0; i < (res.data as []).length; i++) {
-                matchingStreets.push({value: res.data[i].street, label: res.data[i].street})
-            }
-            setOptions(matchingStreets);
-        })
-        // }
-    }, [searchInput])
+    const getOptions = async (value: string) => {
+        const res = await axios.get(`http://localhost:3005/api/street/${value}`);
+        const matchingStreets = [];
+        for (let i = 0; i < (res.data as []).length; i++) {
+            matchingStreets.push({ value: res.data[i].street, label: res.data[i].street });
+        }
+        return matchingStreets;
+    }
 
     return (
         <>
-            <Select placeholder="Introduceti strada..." options={options} onInputChange={input => setSearchInput(input)}></Select>
+            <AsyncSelect cacheOptions placeholder="Introduceti strada..." loadOptions={getOptions} ></AsyncSelect>
         </>
     )
 }
